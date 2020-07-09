3f97adbb0e416b86ff7a3a74384c8398c1c13d76
SC Haw <haw@neverguess.ca>
2020-07-09 12:40:31 -0400

    change toolbar elements to buttons

--- demo/bundle.js
+++ demo/bundle.js
@@ -11,6 +11,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.init = void 0;
+var isBoldToggled = false;
 
 function init(el) {
   el.classList.add("zen-container"); // Initialize editor element
@@ -28,33 +29,110 @@ function createEditor() {
   var editor = document.createElement("div");
   editor.classList.add("zen-editor");
   editor.setAttribute("contentEditable", "true");
-  document.execCommand("defaultParagraphSeparator", false, "p");
   return editor;
 }
 
 function createToolbar() {
   var toolbar = document.createElement("div");
   toolbar.classList.add("zen-toolbar");
-  toolbar.appendChild(createIconButton("format_bold"));
-  toolbar.appendChild(createIconButton("format_italic"));
-  toolbar.appendChild(createIconButton("format_underline"));
-  toolbar.appendChild(createIconButton("strikethrough_s"));
+  toolbar.appendChild(createIconButton("bold-btn", "format_bold", boldSelection));
+  toolbar.appendChild(createIconButton("italic-btn", "format_italic"));
+  toolbar.appendChild(createIconButton("underline-btn", "format_underline"));
+  toolbar.appendChild(createIconButton("strikethrough-btn", "strikethrough_s"));
   return toolbar;
 }
 
-function createIconButton(iconName, onClick) {
+function createIconButton(id, iconName, onClick) {
   if (onClick === void 0) {
     onClick = function () {};
   }
 
-  var btn = document.createElement("div");
-  btn.classList.add("toolbar-btn");
-  var icon = document.createElement("i");
-  icon.classList.add("material-icons");
-  icon.textContent = iconName;
-  icon.onclick = onClick;
-  btn.appendChild(icon);
+  var btn = document.createElement("button");
+  btn.id = id;
+  btn.classList.add("toolbar-btn", "material-icons");
+  btn.textContent = iconName;
+  btn.onclick = onClick;
   return btn;
+}
+
+function boldSelection() {
+  var _a, _b, _c, _d, _e;
+
+  var selection = document.getSelection();
+  var preservedRange = [];
+
+  if (selection && !selection.isCollapsed) {
+    // If range is selected
+    for (var i = 0; i < selection.rangeCount; i++) {
+      var range = selection.getRangeAt(i); // Handle bolding operation
+
+      console.log(range);
+
+      if (range.startContainer.isSameNode(range.endContainer)) {
+        // Simplest case, the selected range belongs to the same container
+        if (((_a = range.startContainer.parentNode) === null || _a === void 0 ? void 0 : _a.nodeName) === "STRONG") {
+          // Unbold text if parent belongs to <strong>
+          if (range.startContainer.textContent) {
+            var textNode = document.createTextNode(range.startContainer.textContent);
+            var nextNode = range.startContainer.parentNode.nextSibling;
+
+            if (nextNode) {
+              range.commonAncestorContainer.insertBefore(textNode, nextNode);
+            } else {
+              range.commonAncestorContainer.appendChild(textNode);
+            } // Remove bolded node
+
+
+            range.commonAncestorContainer.removeChild(range.startContainer.parentNode);
+            var newRange = document.createRange();
+            newRange.selectNodeContents(textNode);
+            preservedRange.push(newRange);
+          }
+        }
+
+        range.surroundContents(document.createElement("strong"));
+      } else {
+        // Traverse through the siblings until the endContainer node
+        var currentNode = range.startContainer;
+
+        while (currentNode) {
+          if (((_b = currentNode.parentNode) === null || _b === void 0 ? void 0 : _b.nodeName) !== "STRONG") {
+            // Create a new range for this node
+            var currRange = document.createRange();
+
+            if (currentNode.isSameNode(range.startContainer)) {
+              currRange.setStart(currentNode, range.startOffset);
+            } else {
+              currRange.setStartBefore(currentNode);
+            }
+
+            if (currentNode.isSameNode(range.endContainer)) {
+              currRange.setEnd(currentNode, range.endOffset);
+            } else {
+              currRange.setEndAfter(currentNode);
+            }
+
+            currRange.surroundContents(document.createElement("strong"));
+            currRange.detach();
+          } else {}
+
+          if (currentNode.isSameNode(range.endContainer)) {
+            break;
+          }
+
+          currentNode = (_c = currentNode.parentNode) === null || _c === void 0 ? void 0 : _c.nextSibling;
+        }
+      }
+    }
+  } else {
+    if (isBoldToggled) {
+      isBoldToggled = false;
+      (_d = document.getElementById("bold-btn")) === null || _d === void 0 ? void 0 : _d.classList.remove("toggled");
+    } else {
+      isBoldToggled = true;
+      (_e = document.getElementById("bold-btn")) === null || _e === void 0 ? void 0 : _e.classList.add("toggled");
+    }
+  }
 }
 
 },{}]},{},[1]);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export interface EditorConfiguration {}
 
+let isBoldToggled = false;
+
 export function init(el: HTMLDivElement): void {
   el.classList.add("zen-container");
   // Initialize editor element
@@ -14,30 +16,103 @@ function createEditor(): HTMLDivElement {
   const editor = document.createElement("div");
   editor.classList.add("zen-editor");
   editor.setAttribute("contentEditable", "true");
-  document.execCommand("defaultParagraphSeparator", false, "p");
   return editor;
 }
 
 function createToolbar(): HTMLDivElement {
   const toolbar = document.createElement("div");
   toolbar.classList.add("zen-toolbar");
-  toolbar.appendChild(createIconButton("format_bold"));
-  toolbar.appendChild(createIconButton("format_italic"));
-  toolbar.appendChild(createIconButton("format_underline"));
-  toolbar.appendChild(createIconButton("strikethrough_s"));
+  toolbar.appendChild(
+    createIconButton("bold-btn", "format_bold", boldSelection)
+  );
+  toolbar.appendChild(createIconButton("italic-btn", "format_italic"));
+  toolbar.appendChild(createIconButton("underline-btn", "format_underline"));
+  toolbar.appendChild(createIconButton("strikethrough-btn", "strikethrough_s"));
   return toolbar;
 }
 
 function createIconButton(
+  id: string,
   iconName: string,
   onClick: (ev: MouseEvent) => any = () => {}
 ): HTMLElement {
-  const btn = document.createElement("div");
-  btn.classList.add("toolbar-btn");
-  const icon = document.createElement("i");
-  icon.classList.add("material-icons");
-  icon.textContent = iconName;
-  icon.onclick = onClick;
-  btn.appendChild(icon);
+  const btn = document.createElement("button");
+  btn.id = id;
+  btn.classList.add("toolbar-btn", "material-icons");
+  btn.textContent = iconName;
+  btn.onclick = onClick;
   return btn;
+}
+
+function boldSelection() {
+  const selection = document.getSelection();
+  const preservedRange = [];
+  if (selection && !selection.isCollapsed) {
+    // If range is selected
+    for (var i = 0; i < selection.rangeCount; i++) {
+      const range = selection.getRangeAt(i);
+      // Handle bolding operation
+      console.log(range);
+      if (range.startContainer.isSameNode(range.endContainer)) {
+        // Simplest case, the selected range belongs to the same container
+        if (range.startContainer.parentNode?.nodeName === "STRONG") {
+          // Unbold text if parent belongs to <strong>
+          if (range.startContainer.textContent) {
+            const textNode = document.createTextNode(
+              range.startContainer.textContent
+            );
+            const nextNode = range.startContainer.parentNode.nextSibling;
+            if (nextNode) {
+              range.commonAncestorContainer.insertBefore(textNode, nextNode);
+            } else {
+              range.commonAncestorContainer.appendChild(textNode);
+            }
+            // Remove bolded node
+            range.commonAncestorContainer.removeChild(
+              range.startContainer.parentNode
+            );
+            preservedRange.push(
+              document.createRange().selectNodeContents(textNode)
+            );
+          }
+        }
+        range.surroundContents(document.createElement("strong"));
+      } else {
+        // Traverse through the siblings until the endContainer node
+        let currentNode: Node | null | undefined = range.startContainer;
+        while (currentNode) {
+          if (currentNode.parentNode?.nodeName !== "STRONG") {
+            // Create a new range for this node
+            const currRange = document.createRange();
+            if (currentNode.isSameNode(range.startContainer)) {
+              currRange.setStart(currentNode, range.startOffset);
+            } else {
+              currRange.setStartBefore(currentNode);
+            }
+            if (currentNode.isSameNode(range.endContainer)) {
+              currRange.setEnd(currentNode, range.endOffset);
+            } else {
+              currRange.setEndAfter(currentNode);
+            }
+            currRange.surroundContents(document.createElement("strong"));
+            currRange.detach();
+          } else {
+          }
+
+          if (currentNode.isSameNode(range.endContainer)) {
+            break;
+          }
+          currentNode = currentNode.parentNode?.nextSibling;
+        }
+      }
+    }
+  } else {
+    if (isBoldToggled) {
+      isBoldToggled = false;
+      document.getElementById("bold-btn")?.classList.remove("toggled");
+    } else {
+      isBoldToggled = true;
+      document.getElementById("bold-btn")?.classList.add("toggled");
+    }
+  }
 }
